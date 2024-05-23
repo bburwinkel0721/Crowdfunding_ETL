c2c69684e5ff6b218565e95838aa775835527fe1
bburwinkel <burwinkelben1@gmail.com>
2024-05-23 19:59:01 -0400

    created query file for testing tables in database

---
+++ select_queries.sql
@@ -0,0 +1,11 @@
+-- Run to verify that all information has been loaded correctly in contacts
+SELECT * FROM contacts;
+
+-- Run to verify that all information has been loaded correctly in category
+SELECT * FROM category;
+
+-- Run to verify that all information has been loaded correctly in subcategory
+SELECT * FROM subcategory;
+
+-- Run to verify that all information has been loaded correctly in campaign
+SELECT * FROM campaign;
